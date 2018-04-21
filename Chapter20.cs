e80465c22ba8621100004b84eaa91f064f854d00
AlexWuDDD <wkjworking@outlook.com>
2018-04-21 20:21:30 +0800

    2018-4-21 8:21 PM

--- Chapter20.cs
+++ Chapter20.cs
@@ -98,6 +98,22 @@ namespace Chapter20
         private static int GetSum(int i1, int i2){return i1 + i2;}
     }
 
+    static class DoStuff
+    {
+        public static async Task<int> FindSeriesSum(int i1)
+        {
+            int sum = 0;
+            for(int i=0; i < i1; ++i)
+            {
+                sum += i;
+                if(i%1000 == 0)
+                    await Task.Yield();
+            }
+
+            return sum;
+        }
+    }
+
 
 
     class Program
@@ -112,6 +128,18 @@ namespace Chapter20
             //ds2.DoRun();
             Console.WriteLine("************************************************");
 
+            Task<int> value = DoStuff.FindSeriesSum(1000000);
+            CountBig(100000);
+            CountBig(100000);
+            CountBig(100000);
+            CountBig(100000);
+            Console.WriteLine("Sum:{0}", value.Result);
+        }
+
+        private static void CountBig(int p)
+        {
+            for(int i = 0; i < p ; ++i)
+                ;
         }
     }
 }
